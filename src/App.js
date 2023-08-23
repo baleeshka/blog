f9b6742b86c31d07902d062a711f9c587655382e
baleeshka <balex.working@gmail.com>
2023-08-23 06:17:21 +0300

    Настроил проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Div>
+			<i className="fa fa-camera-retro fa-5x"></i>
+			Blog
+		</Div>
+	);
 }
 
 export default App;
